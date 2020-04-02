39534a0aae02455fb091ec7fab879038954d6648
Xiaomi <ev.kozelkova>
2020-04-02 19:31:37 +0300

    add repeater

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, request
 import logging
 import json
-from geo import get_country, get_distance, get_coordinates
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO, filename='app.log', format='%(asctime)s %(levelname)s %(name)s %(message)s')
-
 
 @app.route('/post', methods=['POST'])
 def main():
@@ -21,56 +19,33 @@ def main():
         }
     }
 
-    handle_dialog(response, request.json)
+    handler(request.json)
 
     logging.info('Request: %r', response)
 
     return json.dumps(response)
 
 
-def handle_dialog(res, req):
-
-    user_id = req['session']['user_id']
-
-    if req['session']['new']:
-
-        res['response']['text'] = 'Привет! Назови свое имя!'
-
-        return
-
-    cities = get_cities(req)
-
-    if len(cities) == 0:
-
-        res['response']['text'] = 'Ты не написал название не одного города!'
-
-    elif len(cities) == 1:
-
-        res['response']['text'] = 'Этот город в стране - ' + get_country(cities[0])
-
-    elif len(cities) == 2:
-
-        distance = get_distance(get_coordinates(cities[0]), get_coordinates(cities[1]))
-        res['response']['text'] = 'Расстояние между этими городами: ' + str(round(distance)) + ' км.'
-
-    else:
-
-        res['response']['text'] = 'Слишком много городов!'
-
-
-def get_cities(req):
-
-    cities = []
-
-    for entity in req['request']['nlu']['entities']:
-
-        if entity['type'] == 'YANDEX.GEO':
-
-            if 'city' in entity['value'].keys():
-                cities.append(entity['value']['city'])
-
-    return cities
-
+def handler(event):
+    """
+    Точка входа для облачной функции.
+    :param event: содержимое request.json().
+    :param context: информация о текущем контексте выполнения.
+    :return: ответ будет представлен в виде json автоматически.
+    """
+    text = 'Привет, я повторю все, что вы скажете'
+    if 'request' in event and \
+            'original_utterance' in event['request'] \
+            and len(event['request']['original_utterance']) > 0:
+        text = event['request']['original_utterance']
+    return {
+        'version': event['version'],
+        'session': event['session'],
+        'response': {
+            'text': text,
+            'end_session': 'false'
+        },
+    }
 
 if __name__ == '__main__':
     app.run()
